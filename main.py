604a7f2f04de7f0f5a889407e4bb79b3ebd31aa4
Miguel Ponce <juan.ponce@cpavs.mx>
2025-11-28 21:13:06 -0600

    Refactor

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from upload import router as upload_router
 from ask import router as ask_router
 
 app = FastAPI()
-origins = [
-    "https://lector-pdfia-81a7qh1fg-blackkengels-projects.vercel.app",
-    "https://lector-pdfia-git-main-blackkengels-projects.vercel.app"
-    "http://localhost:3000"
-]
+# origins = [
+#     "https://lector-pdfia-81a7qh1fg-blackkengels-projects.vercel.app",
+#     "https://lector-pdfia-git-main-blackkengels-projects.vercel.app"
+#     "http://localhost:3000"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
-    allow_methods=["*"],      
-    allow_headers=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(upload_router)
