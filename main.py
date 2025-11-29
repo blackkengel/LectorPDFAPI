71900ea2b0721b2fedd25a064abdf8e5f3e0cd83
Miguel Ponce <juan.ponce@cpavs.mx>
2025-11-28 18:18:32 -0600

    Api backend

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from upload import router as upload_router
 from ask import router as ask_router
 
 app = FastAPI()
+origins = [
+    "https://lector-pdfia-81a7qh1fg-blackkengels-projects.vercel.app",
+    "http://localhost:3000"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],      
+    allow_headers=["*"], 
 )
 
 app.include_router(upload_router)
